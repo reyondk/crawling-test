c8a629a6e5473a52fba1fddbff328e2707404942
reyondk <reyondk@gmail.com>
2022-02-16 14:03:48 +0900

    테스트 파일명 변경

--- src/test/java/com/example/demo/crawling/service/CrawlingServiceTest.java
+++ src/test/java/com/example/demo/crawling/service/CrawlingServiceTest.java
@@ -1,23 +1,26 @@
 package com.example.demo.crawling.service;
 
-import com.example.demo.crawling.service.ValidService;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.crawling.dto.request.PrintRequest;
+import com.example.demo.crawling.dto.response.PrintResponse;
 import com.example.demo.crawling.code.CrawlingType;
 
 @SpringBootTest
-class ValidServiceImplTest {
+class CrawlingServiceTest {
 	
 	@Autowired
-	private ValidService validService;
-
+	private CrawlingService crawlingService;
+	
 	@Test
-	void 크롤링_출력_유효성_체크_테스트() {
+	void 출력_테스트() {
 		PrintRequest request = new PrintRequest(null, CrawlingType.HTML_IGNORE, 100);
-		validService.valid(request);
+		PrintResponse response = crawlingService.print(request);
+		assertThat(response).isNotNull();
 	}
 
 }
--- src/test/java/com/example/demo/crawling/service/ValidServiceTest.java
+++ src/test/java/com/example/demo/crawling/service/ValidServiceTest.java
@@ -1,26 +1,22 @@
 package com.example.demo.crawling.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.crawling.dto.request.PrintRequest;
-import com.example.demo.crawling.dto.response.PrintResponse;
 import com.example.demo.crawling.code.CrawlingType;
 
 @SpringBootTest
-class CrawlingServiceImplTest {
+class ValidServiceTest {
 	
 	@Autowired
-	private CrawlingService crawlingService;
-	
+	private ValidService validService;
+
 	@Test
-	void 출력_테스트() {
+	void 크롤링_출력_유효성_체크_테스트() {
 		PrintRequest request = new PrintRequest(null, CrawlingType.HTML_IGNORE, 100);
-		PrintResponse response = crawlingService.print(request);
-		assertThat(response).isNotNull();
+		validService.valid(request);
 	}
 
 }
